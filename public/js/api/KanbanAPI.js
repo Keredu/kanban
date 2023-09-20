de78fffc13cebfa8d6fc53f514e508aa0554e965
Juan <juanancaro97@gmail.com>
2023-09-20 21:57:20 +0200

    Feature: SQL statements

--- public/js/api/KanbanAPI.js
+++ public/js/api/KanbanAPI.js
@@ -1,90 +1,33 @@
+import sql from './database.js';
+
 export default class KanbanAPI {
 
-	static getItems(columnId) {
-		const column = read().find(column => column.id == columnId);
-
-		if (!column) {
-			return [];
-		}
-
-		return column.items;
+	static async getItems(mode) {
+        const str = `SELECT * FROM item`;
+        return await sql`SELECT * FROM ${sql(mode)}`;
 	}
 
-    static insertItem(columnId, content){
-        const data = read();
-        const column = data.find(column => column.id == columnId);
-        const item = {
-            id: Math.floor(Math.random() * 100000),
-            content
-        };
-
-        if(!column){
-            throw new Error("Column does not exists");
-        }
-
-        column.items.push(item);
-        save(data);
-
-        return item;
+    static async insertItem(item, mode){
+        return await sql`INSERT INTO ${sql(mode)}  ${sql(item)}`;
     }
 
-    static updateItem(itemId, newProps){
-        const data = read();
-        const [item, currentColumn] = (() => {
-            for (const column of data){
-                const item = column.items.find(item => item.id == itemId);
-                if (item){
-                    return [item,column];
-                }
-            }
-        })();
-
-        if(!item){
-            throw new error("Item not found.");
-        }
-
-        //Update content
-
-        item.content = newProps.content === undefined ? item.content : newProps.content;
-        
-        //Update column and position
-
-        if(
-            newProps.columnId !== undefined && newProps.position !== undefined
-        ) {
-            const targetColumn = data.find(column => column.id == newProps.columnId);
-
-            if(!targetColumn){
-                throw new Error("Target column not found!");
-            }
-            
-            //Delete item from the current column
-            currentColumn.items.splice(currentColumn.items.indexOf(item),1);
-
-            //Move item to current column
-            targetColumn.items.splice(newProps.position,0, item);
-        }
-
-        save(data);
-
+    static async updateItem(itemId, newItem, mode){
+        return await sql`UPDATE ${sql(mode)}  set ${sql(newItem, Object.keys(newItem))} where ${sql(mode + "Id")} = ${itemId}`;
     }
 
-    static deleteItem(itemId){
-        const data = read();
-
-        for(const column of data){
-            const item = column.items.find(item => item.id == itemId);
-
-                if (item){
-                    column.items.splice(column.items.indexOf(item),1);
-                }
-                
-        }
-
-        save(data);
+    static async deleteItem(itemId, mode){
+        return await sql`DELETE FROM ${sql(mode)} WHERE ${sql(mode + "Id")} = ${itemId}`;
+        //return await sql`DELETE FROM ${sql(mode)} WHERE "${sql(mode + "Id")}" = ${itemId}`;
     }
 
 
+}
+
+async function connection(){
+    const file = Bun.file('./public/js/api/conf/credentials.json');
+    const credentials =  await file.json(file);
+    const engine = postgres(`postgres://${credentials["username"]}:${credentials["password"]}@${credentials["host"]}:${credentials["port"]}/${credentials["database"]}`);
+    return engine;
 }
 
 
--- public/js/api/database.js
+++ public/js/api/database.js
@@ -1,29 +1,35 @@
-import postgres from "postgres";
+import postgres from 'postgres'
 
+const file = Bun.file('./public/js/api/conf/credentials.json');
+const credentials = await file.json(file);
+const sql = postgres(`postgres://${credentials["username"]}:${credentials["password"]}@${credentials["host"]}:${credentials["port"]}/${credentials["database"]}`) // will use psql environment variables
+
+export default sql
+
+/* 
 const item = {
     id: 1,
     name: 'Probar bun database',
     description: 'Prueba de bun database'
-}
+};
 
 const item2 = {
     position: 1,
     columnId: 1,
     name: 'Probar BUN',
     description: 'Prueba de BUN'
-}
+};
 
-const columns = ['name', 'description']
+const columns = ['name', 'description'];
 
-const file = Bun.file('./conf/credentials.json')
-const credentials = await file.json(file);
+
 
 const sql = postgres(`postgres://${credentials["username"]}:${credentials["password"]}@${credentials["host"]}:${credentials["port"]}/${credentials["database"]}`);
 
-const read = await sql`SELECT * FROM item`;
+const read = await sql`SELECT * FROM "column"`;
 
 console.log(read);
-/* 
+
 const write = await sql`UPDATE item set ${sql(item, columns)} where "itemId" = ${item.id}`;
 const insert = await sql`INSERT INTO item ${sql(item2)}`
 const deleto = await sql`DELETE FROM item WHERE "itemId" = 1`;
