9b8f0c59e18162625233309e47f404be3c69f3cb
Keredu <luiscortesferre@gmail.com>
2023-09-21 14:18:52 +0200

    fix index.html path

--- index.ts
+++ index.ts
@@ -1,14 +1,28 @@
+const path = require('path');
+
 Bun.serve({
     hostname:'localhost',
     port: 8080,
     fetch: handler,
-})
+});
+
+console.log('Server running at http://localhost:8080/');
 
 const BASE_PATH = './public';
 
 function handler(request: Request): Response {
-    const filePath = BASE_PATH + new URL(request.url).pathname;
-    console.log(filePath);
+    const url = new URL(request.url);
+    console.log(url);
+    let filePath = BASE_PATH;
+    if (url.pathname === '/') {
+        filePath = path.join(filePath, '/index.html');
+    }
+    else {
+        filePath = path.join(filePath, url.pathname);
+    }
+
+    
+    console.log('filePath:', filePath);
     const file = Bun.file(filePath);
     return new Response(file);
 }
