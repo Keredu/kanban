cc56236ff75c08a60e120c3691551cf90bb9024a
Juan <juanancaro97@gmail.com>
2023-10-02 12:10:54 +0200

    Creating utils and refactoring code

--- src/service.js
+++ src/service.js
@@ -86,68 +86,51 @@ class KanbanService{
         return sql`SELECT * FROM ${sql(mode)} ORDER BY ${sql(mode + "Id")} DESC LIMIT 1`;
     }
     
-    /**
-     * This method updates an item. If the item is moved to another column, 
-     * it updates the position of the items in the origin and destiny columns.
-     * If the item is moved to the same column, it updates the position of the items.
-     * 
-     * @param {Int} itemId - the id of the item
-     * @param {Item} newItem - the new item
-     * @param {String} mode - it can be "item" or "column"
-     * 
-     * @returns {Promise} - returns a promise with the result of the request
-     */
-    static updateItem(itemId, newItem, mode){
-        
-        if ('position' in newItem){
-            const result = this.getItemId(itemId, mode);
-            result.then((data) => {
-                const oldItem = data[0];
-                if(oldItem.columnId != newItem.columnId){
-                    const origin = sql`UPDATE ${sql(mode)}  set ${sql("position")} = ${sql("position")} - 1 where ${sql("columnId")} = ${oldItem.columnId} and ${sql("position")} > ${oldItem.position}`;
-                    const destiny = sql`UPDATE ${sql(mode)}  set ${sql("position")} = ${sql("position")} + 1 where ${sql("columnId")} = ${newItem.columnId} and ${sql("position")} >= ${newItem.position}`;
-                    Promise.all([origin, destiny]).then(() => {
-                        const resultSql = sql`UPDATE ${sql(mode)}  set ${sql(newItem, Object.keys(newItem))} where ${sql(mode + "Id")} = ${itemId}`;
-                        return resultSql;
-                    });
-                } else if(oldItem.position != newItem.position) {
-                    let move = null;
-                    if(oldItem.position > newItem.position){
-                        move = sql`UPDATE ${sql(mode)}  set ${sql("position")} = ${sql("position")} + 1 where ${sql("columnId")} = ${oldItem.columnId} and ${sql("position")} >= ${newItem.position} and ${sql("position")} < ${oldItem.position}`;
-                    } else {
-                        move = sql`UPDATE ${sql(mode)}  set ${sql("position")} = ${sql("position")} - 1 where ${sql("columnId")} = ${oldItem.columnId} and ${sql("position")} > ${oldItem.position} and ${sql("position")} <= ${newItem.position}`;
-                    }
-                    move.then(() => {
-                        const resultSql = sql`UPDATE ${sql(mode)}  set ${sql(newItem, Object.keys(newItem))} where ${sql(mode + "Id")} = ${itemId}`;
-                        return resultSql;
-                    });
-                };
-            });
-            return result;
-        } else {
-            const resultSql = sql`UPDATE ${sql(mode)}  set ${sql(newItem, Object.keys(newItem))} where ${sql(mode + "Id")} = ${itemId}`;
-            return resultSql;
+    static async updateItem(itemId, newItem, mode) {
+        if (!('position' in newItem)) {
+            return sql`UPDATE ${sql(mode)} set ${sql(newItem, Object.keys(newItem))} where ${sql(mode + "Id")} = ${itemId}`;
         }
+        
+        const oldItem = (await this.getItemId(itemId, mode))[0];
+        if (oldItem.columnId !== newItem.columnId) {
+            await this.updatePositionAcrossColumns(oldItem, newItem, mode);
+        } else if (oldItem.position !== newItem.position) {
+            await this.updatePositionWithinSameColumn(oldItem, newItem, mode);
+        }
+        
+        return sql`UPDATE ${sql(mode)} set ${sql(newItem, Object.keys(newItem))} where ${sql(mode + "Id")} = ${itemId}`;
     }
     
+    static async updatePositionAcrossColumns(oldItem, newItem, mode) {
+        const origin = sql`UPDATE ${sql(mode)} set ${sql("position")} = ${sql("position")} - 1 where ${sql("columnId")} = ${oldItem.columnId} and ${sql("position")} > ${oldItem.position}`;
+        const destiny = sql`UPDATE ${sql(mode)} set ${sql("position")} = ${sql("position")} + 1 where ${sql("columnId")} = ${newItem.columnId} and ${sql("position")} >= ${newItem.position}`;
+        await Promise.all([origin, destiny]);
+    }
+    
+    static async updatePositionWithinSameColumn(oldItem, newItem, mode) {
+        const positionShift = oldItem.position > newItem.position
+            ? sql`UPDATE ${sql(mode)} set ${sql("position")} = ${sql("position")} + 1 where ${sql("columnId")} = ${oldItem.columnId} and ${sql("position")} >= ${newItem.position} and ${sql("position")} < ${oldItem.position}`
+            : sql`UPDATE ${sql(mode)} set ${sql("position")} = ${sql("position")} - 1 where ${sql("columnId")} = ${oldItem.columnId} and ${sql("position")} > ${oldItem.position} and ${sql("position")} <= ${newItem.position}`;
+    
+        await positionShift;
+    }
+
     /**
      * This method deletes an item. It updates the position of the items in the same column.
      * 
      * @param {Int} itemId - the id of the item
      * @param {String} mode - it can be "item" or "column"
      */
-    static deleteItem(itemId, mode){
-        const result = this.getItemId(itemId, mode);
-            result.then((data) => {
-                const oldItem = data[0];
-                const moveup = sql`UPDATE ${sql(mode)} set ${sql("position")} = ${sql("position")} - 1 where ${sql("columnId")} = ${oldItem.columnId} and ${sql("position")} > ${oldItem.position}`;
-                moveup.then(() => {
-                    const resultSql = sql`DELETE FROM ${sql(mode)} WHERE ${sql(mode + "Id")} = ${itemId}`;
-                    return resultSql;
-                });
-            });
-        return result;
+    static async deleteItem(itemId, mode) {
+        const data = await this.getItemId(itemId, mode);
+        const oldItem = data[0];
+    
+        await sql`UPDATE ${sql(mode)} SET ${sql("position")} = ${sql("position")} - 1 WHERE ${sql("columnId")} = ${oldItem.columnId} AND ${sql("position")} > ${oldItem.position}`;
+        
+        const resultSql = sql`DELETE FROM ${sql(mode)} WHERE ${sql(mode + "Id")} = ${itemId}`;
+        return resultSql;
     }
+
 }
 
 module.exports = KanbanService;
